f171ecab2ebe9055be4cc9467917081dbd0f7a61
ahsan1999wd <ahsan1999wd@gmail.com>
2022-08-29 23:31:21 +0600

    more js add

---
+++ module-31-more-es6/6ways-to-loop-through-an-array.js
@@ -0,0 +1,42 @@
+/*---------- for loop --------------*/
+
+// let array = [10,20,30,40];
+// for (let i = 0; i <= array.length; i++)
+// {
+//   console.log(array[i]);
+// }
+
+/*--------------- do while ---------------*/
+
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5)
+
+/*--------------- while loop ---------------*/
+
+// let array = [10,20,30,40];
+// let i = 0;
+// while (i < array.length){
+//   console.log(array[i]);
+//   i++;
+// };
+
+/*--------------- for of ---------------*/
+
+// let array = [10,20,30,40];
+// for (let i of array)
+// {
+//   console.log(i);
+// }
+
+/*---------------- map() method ----------------*/
+
+// const names =['Lincoln','Daniel','Tabitha','McLeod']
+// let newArr = names.map((index) => console.log(index));
+
+// /*--------------- forEach() ---------------*/
+
+// let array = [10,20,30,40];
+// array.forEach((index) => {console.log(index)});
---
+++ module-31-more-es6/js/notation.js
@@ -0,0 +1,38 @@
+// const student = {
+//   name: 'Kolim Uddin',
+//   age: 15,
+//   class: 'Ten',
+//   marks: {
+//       math: 78,
+//       physics: 89,
+//       chemistry: 65
+//   },
+
+// }
+
+// const marks = student.marks;
+// const math = student.marks.math;
+
+// /*----------------------------------------------------*/
+
+// const chemistry = student['marks']['chemistry'];
+// const subject = 'math';
+// const subjectMarks = student.marks[subject];
+// console.log(subjectMarks);
+
+/*-----------------------------------------------------*/
+
+const variable = 'happy';
+const emotion = {
+  happy: 'H',
+  sad: 'S'
+};
+
+// Dot : Access Property   <-----!!!
+emotion.happy;
+// 'H'
+
+
+// Bracket : Access Property With Variable   <-----!!!
+emotion[variable];
+// 'H'
