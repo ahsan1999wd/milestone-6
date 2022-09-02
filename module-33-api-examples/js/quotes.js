4e3dcbbe11108afef919af3d2ef4936351827c59
ahsan1999wd <ahsan1999wd@gmail.com>
2022-09-02 22:25:06 +0600

    new file

---
+++ module-33-api-examples/js/quotes.js
@@ -0,0 +1,22 @@
+/*----------------------------------------------
+const loadQuote = () =>{
+  fetch('https://api.kanye.rest/')
+  .then(res => res.json())
+  .then( data => console.log(data))  
+}
+
+loadQuote();
+
+/*----------------------------------------------*/
+
+const loadQuote = () =>{
+  fetch('https://api.kanye.rest/')
+  .then(res => res.json())
+  .then(data => displayQuote(data))
+}
+
+const displayQuote = quote => {
+  const blockQuote = document.getElementById('quote');
+  console.log(quote);
+  blockQuote.innerText = quote.quote;
+}
